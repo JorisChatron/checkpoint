7fb5945eaecd9c28652c53d2dc3f99c3f4006474
JorisChatron <joris.chatron.colliet@gmail.com>
2025-04-30 15:33:48 +0200

    ok

--- app/Views/layouts/navbar.php
+++ app/Views/layouts/navbar.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Page d'accueil</title>
+<title>Checkpoint</title>
 
 <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
 
