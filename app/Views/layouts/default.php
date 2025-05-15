42b585dce37ed4a4fff904b48845bb33e3f9788e
JorisChatron <joris.chatron.colliet@gmail.com>
2025-05-15 14:03:31 +0200

    màj icon

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= esc($title ?? 'Checkpoint') ?></title>
+    <link rel="icon" type="image/png" href="<?= base_url('images/icon.png') ?>" sizes="32x32">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('public/css/style.css') ?>">
     <script defer>
@@ -16,13 +17,11 @@
     </script>
 </head>
 <body>
-
     <?= $this->include('layouts/navbar') ?>
 
     <main>
         <?= $this->renderSection('content') ?>
     </main>
-
 </body>
 </html>
 
